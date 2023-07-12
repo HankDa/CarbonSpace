df81b36bc68742699cc04930aedff0f76d342f8c
HankDa <muhd0416@gmail.com>
2023-07-12 22:21:15 +0100

    prase the temperature data from dataframe.

--- geoJonProcessor.py
+++ geoJonProcessor.py
@@ -1,52 +1,36 @@
 import cdsapi
 import geopandas as gpd
+import math
 from datetime import datetime, timedelta
 
 
 class TemperatureDataDownloader:
-    def __init__(self, api_key):
-        self.cds_client = cdsapi.Client(key=api_key)
+    def __init__(self):
+        self.cds_client = cdsapi.Client()
 
     def download_temperature_data(self, bbox, start_date, end_date):
-        # Implement the logic to download temperature data from the CDS API
-        # TODO: months/area should not be hard coding. And how to handle the downloaded data. 
-        c = cdsapi.Client()
+        # Generate a list of months between start_date and end_date
+        months = [str((start_date + timedelta(days=i)).month).zfill(2) for i in range((end_date - start_date).days + 1)]
 
-        c.retrieve(
+        # Generate a list of days between start_date and end_date
+        days = [str((start_date + timedelta(days=i)).day).zfill(2) for i in range((end_date - start_date).days + 1)]
+
+        self.cds_client.retrieve(
             'sis-agrometeorological-indicators',
             {
                 'variable': '2m_temperature',
                 'statistic': '24_hour_mean',
-                'year': '2023',
-                'month': [
-                    '01', '02', '03',
-                    '04', '05', '06',
-                    '07',
-                ],
-                'day': [
-                        '01', '02', '03',
-                        '04', '05', '06',
-                        '07', '08', '09',
-                        '10', '11', '12',
-                        '13', '14', '15',
-                        '16', '17', '18',
-                        '19', '20', '21',
-                        '22', '23', '24',
-                        '25', '26', '27',
-                        '28', '29', '30',
-                        '31',
-                ],
+                'year': str(start_date.year),
+                'month': months,
+                'day': days,
                 'area': [
-                    bbox[3], bbox[0], bbox[1],
-                    bbox[2],
+                    math.ceil(bbox[3]), math.floor(bbox[0]), math.floor(bbox[1]), math.ceil(bbox[2])
                 ],
                 'format': 'tgz',
             },
-            'download.tar.gz')
-        
-    def date_period():
-        pass
-        
+            'download.tar.gz'
+        )
+
 
 class GeoJSONProcessor:
     def __init__(self, file_path):
@@ -56,16 +40,17 @@ class GeoJSONProcessor:
         self.centroids = None
 
     def load_geojson_file(self):
-        self.gdf = gpd.read_file('test_features.geojson') 
+        self.gdf = gpd.read_file(self.file_path) 
         # validating the gdf
 
     def calculate_bbox(self):
         # Implement the logic to calculate the bbox covering all features
+        # minx, miny, maxx, maxy (W,S,E,N)
         self.bbox = self.gdf.total_bounds
         
     def find_feature_centroids(self):
         # Implement the logic to find centroids for each shape in the GeoJSON file
-        self.centroids = self.gdf.geometry
+        self.centroids = self.gdf.geometry.centroid
         
     def identify_nearest_datapoint(self, centroids):
         # Implement the logic to identify the nearest available temperature datapoint
@@ -86,9 +71,9 @@ class GeoJSONProcessor:
 
 if __name__ == '__main__':
     # Set up the necessary parameters and instantiate the classes
-    api_key = 'YOUR_CDS_API_KEY'
-    downloader = TemperatureDataDownloader(api_key)
-    geojson_processor = GeoJSONProcessor('path/to/geojson/file')
+    file_path = 'test_features.geojson'
+    downloader = TemperatureDataDownloader()
+    geojson_processor = GeoJSONProcessor(file_path)
 
     # Load and process the GeoJSON file
     geojson_processor.load_geojson_file()
@@ -96,13 +81,13 @@ if __name__ == '__main__':
     centroids = geojson_processor.find_feature_centroids()
 
     # Download temperature data for the calculated bbox
-    bbox = geojson_processor.get_calculated_bbox()
+    bbox = geojson_processor.calculate_bbox()
     start_date = datetime(2023, 1, 1)
     end_date = datetime.now()
     downloader.download_temperature_data(bbox, start_date, end_date)
 
     # Process and update the GeoJSON file with temperature data
     geojson_processor.identify_nearest_datapoint(centroids)
-    geojson_processor.aggregate_monthly_average()
-    geojson_processor.update_geojson_properties()
-    geojson_processor.write_updated_geojson_file()
+    # geojson_processor.aggregate_monthly_average()
+    # geojson_processor.update_geojson_properties()
+    # geojson_processor.write_updated_geojson_file()
