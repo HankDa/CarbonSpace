486eedea56231d773ea4df51ed4c5cdc06b54e71
HankDa <muhd0416@gmail.com>
2023-07-17 15:46:36 +0100

    Finished the single thread version.

--- geoJsonProcessor.py
+++ geoJsonProcessor.py
@@ -13,56 +13,35 @@ import os
 import argparse
 
 
-
-def list_days_of_month(start_date: datetime, end_date: datetime) -> dict:
-    # Implement the logic to list the days of a month
-    date_dict = {}
-    current_date = start_date
-    while current_date <= end_date:
-        year = str(current_date.year).zfill(2)
-        month = str(current_date.month).zfill(2)
-        day = str(current_date.day).zfill(2)
-
-        if (month, year) not in date_dict:
-            date_dict[(month, year)] = []
-
-        date_dict[(month, year)].append(day)
-
-        current_date += timedelta(days=1)
-    return date_dict
-
-
 class TemperatureDataDownloader:
     def __init__(self):
         self.cds_client = cdsapi.Client()
         self.downloaded_tar = []
-        # {month-year: [nc_file_names_1, ...], month-year: [nc_file_names_2, ...], ...}
+        # {YYYMM: [nc_file_names_1, ...], YYYMM: [nc_file_names_2, ...], ...}
         self.nc_file_list_by_month = {}
-    # hide the download log
-    def download_temperature_data(self, bbox: list, start_date: datetime, end_date: datetime):
-        days_of_month_list = list_days_of_month(start_date, end_date)
-        for month, year in days_of_month_list:
-            try:
-                tar_name = f'{year}{month}'
-                self.cds_client.retrieve(
-                    'sis-agrometeorological-indicators',
-                    {
-                        'variable': '2m_temperature',
-                        'statistic': '24_hour_mean',
-                        'year': year,
-                        'month': month,
-                        'day': days_of_month_list[(month, year)],
-                        'area': [
-                            ceil(bbox[3]), floor(bbox[0]), floor(bbox[1]), ceil(bbox[2])
-                        ],
-                        'format': 'tgz',
-                    },
-                    f'{tar_name}.tar.gz'
-                )
-                self._list_fileName(tar_name)
-                print(f'Downloaded files: {tar_name}.tar.gz')
-            except Exception as e:
-                print(f'Error downloading the data: {e}')
+    # TODO: Hide the download log    
+    def download_temperature_data(self, bbox: list, year: str, month: str, days: list[str]):
+        try:
+            tar_name = f'{year}{month}'
+            self.cds_client.retrieve(
+                'sis-agrometeorological-indicators',
+                {
+                    'variable': '2m_temperature',
+                    'statistic': '24_hour_mean',
+                    'year': year,
+                    'month': month,
+                    'day': days,
+                    'area': bbox,
+                    'format': 'tgz',
+                },
+                f'{tar_name}.tar.gz'
+            )
+            # uncompress the .tar.gz file and list the files name
+            self._list_fileName(tar_name)
+            print(f'Downloaded files: {tar_name}.tar.gz')
+
+        except Exception as e:
+            print(f'Error downloading the data: {e}')
 
     def _list_fileName(self, tar_name):
         with tarfile.open(tar_name+'.tar.gz', 'r:gz') as tar:
@@ -79,11 +58,28 @@ class TemperatureDataDownloader:
 class GeoJSONProcessor:
     def __init__(self, file_path: str):
         self.file_path: str = file_path
-        self.gdf = self._load_geojson_file()
-        self.bbox = self._calculate_bbox()
-        self.df_centroids = self._find_features_centroids()
+        self.gdf: gpd.GeoDataFrame = self._load_geojson_file()
+        self.bbox: list = self._calculate_bbox()
+        self.df_centroids: pd.DataFrame = self._find_features_centroids()
         self.df_monthly_average_temp = pd.DataFrame(self.df_centroids["name"])
     
+    def __str__(self):
+        return (
+            f"{'='*50}\n"
+            f"* GeoJSON file: {self.file_path}\n"
+            f"{'='*50}\n"
+            f"* Features (head):\n"
+            f"{self.gdf.head()}\n"
+            f"{'='*50}\n"
+            f"* Bounding Box: {self.bbox}\n"
+            f"{'='*50}\n"
+            f"* Centroids of features (head): \n"
+            f"{self.df_centroids.head()}\n"
+            f"{'='*50}\n"
+            f"* Monthly average temperature: \n"
+            f"{self.df_monthly_average_temp.head()}\n"
+        )
+        
     # validating the gdf
     def _load_geojson_file(self):
         try: 
@@ -94,7 +90,7 @@ class GeoJSONProcessor:
 
     def _calculate_bbox(self):
         # Implement the logic to calculate the bbox covering all features
-        # minx, miny, maxx, maxy (W,S,E,N)
+        # [minx, miny, maxx, maxy] -> (W,S,E,N)
         return self.gdf.total_bounds
         
     def _find_features_centroids(self):
@@ -109,7 +105,6 @@ class GeoJSONProcessor:
         # monthly_averages: list -> [avg_temperature_value_01, ...]
         self.df_monthly_average_temp[month] = self._aggregate_monthly_average(daily_temperature_by_month, month)
     
-        
     def _update_centroids_with_temperature(self, monthly_averages):
         for centroid_idx in range(len(self.df_centroids.centroid)):
             nearest_idx = self.df_centroids.nearest_idx.iloc[centroid_idx]
@@ -156,7 +151,6 @@ class GeoJSONProcessor:
     def _update_geojson_properties(self):
         # Implement the logic to update the GeoJSON properties with temperature data
         self.gdf = pd.merge(self.gdf, self.df_monthly_average_temp, on='name', how='outer')
-        print(self.gdf)
 
     def write_updated_geojson_file(self):
         # Implement the logic to write the updated GeoJSON file
@@ -164,23 +158,40 @@ class GeoJSONProcessor:
         self.gdf.to_file('result.geojson', driver="GeoJSON")  
         
 
+def list_days_of_month(start_date: datetime, end_date: datetime) -> dict:
+    # Implement the logic to list the days of a month
+    #{(year, month): [day_1, day_2, ...], (year, month): [day_1, day_2, ...], ...}
+    date_dict = defaultdict(list)
+    for date in pd.date_range(start_date, end_date):
+        date_dict[(str(date.year).zfill(2), str(date.month).zfill(2))].append(str(date.day).zfill(2))
 
-def main(file_path):
+    return date_dict
+
+
+def main_singleThread(file_path):
     downloader = TemperatureDataDownloader()
     geojson_processor = GeoJSONProcessor(file_path)
 
-    bbox = geojson_processor.bbox
     start_date = datetime(2023, 1, 1)
     end_date = datetime.now()
-
+    days_of_month_list = list_days_of_month(start_date, end_date)
+    bbox = geojson_processor.bbox
+    # bbox[0] = W, bbox[1] = S, bbox[2] = E, bbox[3] = N, and I just found if request the float bbox it will download the whole area. 
+    bbox_for_download = [ceil(bbox[3]), floor(bbox[0]), floor(bbox[1]), ceil(bbox[2])]
+    
+    download_times = {}
     start_time = time.time()
-    downloader.download_temperature_data(geojson_processor.bbox, start_date, end_date)
-    download_time = time.time() - start_time
+    for year, month in days_of_month_list:
+        days = days_of_month_list[(year, month)]
+        start_time = time.time()
+        downloader.download_temperature_data(bbox_for_download, year, month, days)
+        download_time = time.time() - start_time
+        download_times[(year, month)] = download_time
 
     processing_times = {}
     for month in downloader.nc_file_list_by_month:
         start_time = time.time()
-        geojson_processor.get_monthly_avg_temperature(downloader.nc_file_list_by_month, month)
+        geojson_processor.get_monthly_avgTemperature(downloader.nc_file_list_by_month, month)
         processing_time = time.time() - start_time
         processing_times[month] = processing_time
 
@@ -188,17 +199,38 @@ def main(file_path):
     geojson_processor.write_updated_geojson_file()
     write_time = time.time() - start_time
 
-    print("Processing Times:")
-    print("=================")
-    print(f"Download Time: {download_time:.2f} seconds")
-    for month, time in processing_times.items():
-        print(f"Processing Time for {month}: {time:.2f} seconds")
-    print(f"Write Time: {write_time:.2f} seconds")
+    print(f"{'*' * 10} Result {'*' * 10}")
+    print(f"{'='*50}")
+    print(f"* Start Date: {start_date}")
+    print(f"* End Date: {end_date}")
+    print(str(geojson_processor))
+    print(f"{'='*50}")
+    print(f"* Download Time:")
+    print(f"{'='*50}")
+    total_download_time = 0
+    for (year, month), download_time in download_times.items():
+        total_download_time += download_time
+        print(f"Download Time for {month}-{year}: {download_time:.2f} seconds")
+    print(f"-> Total Download Time: {total_download_time:.2f} seconds")
+    print(f"{'='*50}")
+    print(f"* Processing Time:")
+    print(f"{'='*50}")
+    total_processing_time = 0
+    for month, processing_time in processing_times.items():
+        total_processing_time += processing_time
+        print(f"Processing Time for {month}: {processing_time:.2f} seconds")
+    print(f"-> Total Processing Time: {total_processing_time:.2f} seconds")
+    print(f"{'='*50}")
+    print(f"* Write Time: {write_time:.2f} seconds")
+    print(f"{'='*50}")
+    print(f"* Total Run Time: {total_download_time+total_processing_time+write_time:.2f} seconds")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process temperature data.')
     parser.add_argument('file_path', type=str, help='Path to the GeoJSON file')
     args = parser.parse_args()
 
-    main(args.file_path)
+    main_singleThread(args.file_path)
+
+    # main_singleThread('test_features.geojson')
     
