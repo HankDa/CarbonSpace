8c491c4c5dfd919ce59a894cca41aea51e3e97f2
HankDa <muhd0416@gmail.com>
2023-07-17 15:58:40 +0100

    Add doc string.

--- geoJsonProcessor.py
+++ geoJsonProcessor.py
@@ -14,13 +14,24 @@ import argparse
 
 
 class TemperatureDataDownloader:
+    """Class to download temperature data using the Climate Data Store (CDS) API."""
+
     def __init__(self):
         self.cds_client = cdsapi.Client()
         self.downloaded_tar = []
-        # {YYYMM: [nc_file_names_1, ...], YYYMM: [nc_file_names_2, ...], ...}
         self.nc_file_list_by_month = {}
-    # TODO: Hide the download log    
+
     def download_temperature_data(self, bbox: list, year: str, month: str, days: list[str]):
+        """
+        Download temperature data for a specific month and days within that month.
+
+        Args:
+            bbox (list): Bounding box coordinates [minx, miny, maxx, maxy].
+            year (str): Year.
+            month (str): Month.
+            days (list[str]): List of days within the month.
+
+        """
         try:
             tar_name = f'{year}{month}'
             self.cds_client.retrieve(
@@ -36,7 +47,6 @@ class TemperatureDataDownloader:
                 },
                 f'{tar_name}.tar.gz'
             )
-            # uncompress the .tar.gz file and list the files name
             self._list_fileName(tar_name)
             print(f'Downloaded files: {tar_name}.tar.gz')
 
@@ -44,19 +54,32 @@ class TemperatureDataDownloader:
             print(f'Error downloading the data: {e}')
 
     def _list_fileName(self, tar_name):
+        """
+        Extract file names from a .tar.gz file and group them by month.
+
+        Args:
+            tar_name (str): Name of the .tar.gz file.
+
+        """
         with tarfile.open(tar_name+'.tar.gz', 'r:gz') as tar:
-            # List all file names in the .tar.gz file
             file_names = tar.getnames()
-            # uncompress the .tar.gz file
             tar.extractall(tar_name)
         os.remove(tar_name + '.tar.gz')
-        # Group the file names by date
         file_names = sorted(file_names)
         self.nc_file_list_by_month[tar_name] = file_names
 
 
 class GeoJSONProcessor:
+    """Class to process GeoJSON files and calculate monthly average temperature."""
+
     def __init__(self, file_path: str):
+        """
+        Initialize GeoJSONProcessor.
+
+        Args:
+            file_path (str): Path to the GeoJSON file.
+
+        """
         self.file_path: str = file_path
         self.gdf: gpd.GeoDataFrame = self._load_geojson_file()
         self.bbox: list = self._calculate_bbox()
@@ -64,6 +87,13 @@ class GeoJSONProcessor:
         self.df_monthly_average_temp = pd.DataFrame(self.df_centroids["name"])
     
     def __str__(self):
+        """
+        Return string representation of GeoJSONProcessor.
+
+        Returns:
+            str: String representation.
+
+        """
         return (
             f"{'='*50}\n"
             f"* GeoJSON file: {self.file_path}\n"
@@ -80,67 +110,114 @@ class GeoJSONProcessor:
             f"{self.df_monthly_average_temp.head()}\n"
         )
         
-    # validating the gdf
-    def _load_geojson_file(self):
+    def _load_geojson_file(self) -> gpd.GeoDataFrame:
+        """
+        Load the GeoJSON file.
+
+        Returns:
+            gpd.GeoDataFrame: GeoDataFrame containing the loaded GeoJSON file.
+
+        Raises:
+            Exception: If there is an error loading the GeoJSON file.
+
+        """
         try: 
             gdf = gpd.read_file(self.file_path) 
         except Exception as e:
             raise Exception(f'Error loading the GeoJSON file: {e}')
         return gdf
 
-    def _calculate_bbox(self):
-        # Implement the logic to calculate the bbox covering all features
-        # [minx, miny, maxx, maxy] -> (W,S,E,N)
+    def _calculate_bbox(self) -> list:
+        """
+        Calculate the bounding box covering all features.
+
+        Returns:
+            list: Bounding box coordinates [minx, miny, maxx, maxy].
+
+        """
         return self.gdf.total_bounds
         
     def _find_features_centroids(self):
+        """
+        Find the centroids of the features.
+
+        Returns:
+            pd.DataFrame: DataFrame containing the feature names and their centroids.
+
+        """
         df = pd.DataFrame(columns=['name', 'centroid', 'nearest_point', 'nearest_idx'])
         centroids = self.gdf.geometry.centroid.rename("centroid")
         df = pd.concat([self.gdf['name'], centroids], axis=1)
         return df
 
-    def get_monthly_avgTemperature(self, nc_file_list_by_month, month):
-        # daily_temperature_by_month: dict -> {month: {centroid_idx:[tempertature_value, ...], ... }
+    def get_monthly_avg_temperature(self, nc_file_list_by_month: dict, month: str):
+        """
+        Calculate the monthly average temperature.
+
+        Args:
+            nc_file_list_by_month (dict): Dictionary containing the list of netCDF file names by month.
+            month (str): Month for which to calculate the average temperature.
+
+        """
         daily_temperature_by_month = self._daily_temperature_by_month(nc_file_list_by_month, month)
-        # monthly_averages: list -> [avg_temperature_value_01, ...]
         self.df_monthly_average_temp[month] = self._aggregate_monthly_average(daily_temperature_by_month, month)
-    
-    def _update_centroids_with_temperature(self, monthly_averages):
-        for centroid_idx in range(len(self.df_centroids.centroid)):
-            nearest_idx = self.df_centroids.nearest_idx.iloc[centroid_idx]
-            temperature_value = monthly_averages[nearest_idx[1]][nearest_idx[0]]
-            self.df_centroids.at[centroid_idx, 'temperature'] = temperature_value
-        return self.df_centroids
-    
+
     def _daily_temperature_by_month(self, nc_file_list_by_month: dict, month) -> dict:
-        
+        """
+        Retrieve the daily temperature values for each centroid point in a given month.
+
+        Args:
+            nc_file_list_by_month (dict): Dictionary containing the list of netCDF file names by month.
+            month (str): Month for which to retrieve the temperature values.
+
+        Returns:
+            dict: Dictionary with daily temperature values for each centroid.
+
+        """
         daily_temperature_by_month = defaultdict(lambda: defaultdict(list))        
         for file_name in nc_file_list_by_month[month]:
-            # the folder named as the month e.g: 01-2023
             dataset = nc.Dataset(month+'/'+file_name)
-            # Get the latitude and longitude variables
             lat = dataset.variables['lat'][:]
             lon = dataset.variables['lon'][:]
             
             for centroid_idx in range(len(self.df_centroids.centroid)):
                 nearest_idx = self._identify_nearest_datapoint(lat, lon, centroid_idx)
-                # Convert the temperature from Kelvin to Celsius
                 temperature_value = dataset['Temperature_Air_2m_Mean_24h'][0][nearest_idx[1]][nearest_idx[0]] - 273.15
                 daily_temperature_by_month[month][centroid_idx].append(temperature_value)
         return daily_temperature_by_month
         
-    # ISSUE: as the Horizontal resolution = 0.1° x 0.1°, the nearest points will be same for each centroid point.
-    def _identify_nearest_datapoint(self, lat, lon, centroid_idx: int)-> tuple:
-        # Implement the logic to identify the nearest available temperature datapoint
+    def _identify_nearest_datapoint(self, lat, lon, centroid_idx: int) -> tuple:
+        """
+        Identify the nearest available temperature datapoint for a centroid.
+
+        Args:
+            lat (np.ndarray): Array of latitude values.
+            lon (np.ndarray): Array of longitude values.
+            centroid_idx (int): Index of the centroid.
+
+        Returns:
+            tuple: Indices of the nearest datapoint (lon_index, lat_index).
+
+        """
         centroid_point = self.df_centroids.centroid.iloc[centroid_idx]
         lat_index = (abs(lat - centroid_point.y)).argmin()
         lon_index = (abs(lon - centroid_point.x)).argmin()
         self.df_centroids.at[centroid_idx, 'nearest_point'] = Point(lon[lon_index], lat[lat_index])
-        # Issue: Must have equal len keys and value when setting with an iterable
         self.df_centroids.at[centroid_idx, 'nearest_idx'] = Point(lon_index, lat_index)
         return (lon_index, lat_index)
 
-    def _aggregate_monthly_average(self, daily_temperature_by_month, month)-> pd.Series:
+    def _aggregate_monthly_average(self, daily_temperature_by_month, month) -> pd.Series:
+        """
+        Calculate the monthly average temperature for each centroid.
+
+        Args:
+            daily_temperature_by_month (dict): Dictionary with daily temperature values for each centroid.
+            month (str): Month for which to calculate the average.
+
+        Returns:
+            pd.Series: Series with monthly average temperature values.
+
+        """
         monthly_averages = []
         for centroid, temperatures in daily_temperature_by_month[month].items():
             avg_temperature = sum(temperatures) / len(temperatures)
@@ -149,18 +226,33 @@ class GeoJSONProcessor:
 
 
     def _update_geojson_properties(self):
-        # Implement the logic to update the GeoJSON properties with temperature data
+        """
+        Update the GeoJSON properties with temperature data.
+
+        """
         self.gdf = pd.merge(self.gdf, self.df_monthly_average_temp, on='name', how='outer')
 
     def write_updated_geojson_file(self):
-        # Implement the logic to write the updated GeoJSON file
+        """
+        Write the updated GeoJSON file.
+
+        """
         self._update_geojson_properties()
         self.gdf.to_file('result.geojson', driver="GeoJSON")  
         
 
 def list_days_of_month(start_date: datetime, end_date: datetime) -> dict:
-    # Implement the logic to list the days of a month
-    #{(year, month): [day_1, day_2, ...], (year, month): [day_1, day_2, ...], ...}
+    """
+    List the days of each month within a given date range.
+
+    Args:
+        start_date (datetime): Start date.
+        end_date (datetime): End date.
+
+    Returns:
+        dict: Dictionary with month and days for each month.
+
+    """
     date_dict = defaultdict(list)
     for date in pd.date_range(start_date, end_date):
         date_dict[(str(date.year).zfill(2), str(date.month).zfill(2))].append(str(date.day).zfill(2))
@@ -169,6 +261,13 @@ def list_days_of_month(start_date: datetime, end_date: datetime) -> dict:
 
 
 def main_singleThread(file_path):
+    """
+    Main function to run the single-threaded processing.
+
+    Args:
+        file_path (str): Path to the GeoJSON file.
+
+    """
     downloader = TemperatureDataDownloader()
     geojson_processor = GeoJSONProcessor(file_path)
 
@@ -176,11 +275,9 @@ def main_singleThread(file_path):
     end_date = datetime.now()
     days_of_month_list = list_days_of_month(start_date, end_date)
     bbox = geojson_processor.bbox
-    # bbox[0] = W, bbox[1] = S, bbox[2] = E, bbox[3] = N, and I just found if request the float bbox it will download the whole area. 
     bbox_for_download = [ceil(bbox[3]), floor(bbox[0]), floor(bbox[1]), ceil(bbox[2])]
     
     download_times = {}
-    start_time = time.time()
     for year, month in days_of_month_list:
         days = days_of_month_list[(year, month)]
         start_time = time.time()
@@ -191,7 +288,7 @@ def main_singleThread(file_path):
     processing_times = {}
     for month in downloader.nc_file_list_by_month:
         start_time = time.time()
-        geojson_processor.get_monthly_avgTemperature(downloader.nc_file_list_by_month, month)
+        geojson_processor.get_monthly_avg_temperature(downloader.nc_file_list_by_month, month)
         processing_time = time.time() - start_time
         processing_times[month] = processing_time
 
@@ -224,6 +321,7 @@ def main_singleThread(file_path):
     print(f"* Write Time: {write_time:.2f} seconds")
     print(f"{'='*50}")
     print(f"* Total Run Time: {total_download_time+total_processing_time+write_time:.2f} seconds")
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process temperature data.')
